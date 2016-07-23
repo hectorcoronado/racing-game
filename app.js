ef4d6d0e2f7cb246a5ed10b06ac838f44b2940ac
hectorcoronado <coronado.hector@gmail.com>
2016-07-22 18:51:21 -0700

    blue player position recorded

--- app.js
+++ app.js
@@ -16,46 +16,18 @@ $(document).on('ready', function() {
 
   $(window).keyup(function () {
     if (event.which === 76) {
-      console.log ("Ok, we're listening for L");
-
-      //How far blue needs to go
-      var blueTrackWidth = ( $('#blueTrack').width() );
-      console.log ("Blue track width: " + blueTrackWidth);
-
-      //To determine red position's value
-      var bluePlayerMargin = ( parseInt( $('.bluePlayer').css('left') ) );
-      console.log ("Blue is currently at: " + bluePlayerMargin);
-
-      //Possible solution would be to set winning condition to
-      //'position', not to ''
-
-      $('.bluePlayer').animate ( { 'left' : '+=75'}, 'linear');
-
-      if (bluePlayerMargin >= blueTrackWidth - 400) {
-        alert ('You win, big blue pixel!');
-        $('.pop-up').show(250);
-      }
-
+      var bluePlayerPosition = parseInt( $('.bluePlayer').css('left') );
+      console.log ('Blue player position is: ' + bluePlayerPosition);
+      $('.bluePlayer').animate ( { 'left' : '+=20'}, 50);
     }
   });
 
 });
 
-// extracted logic from redPlayer:
 
-//   //To determine how far red needs to go
-//   var redTrackWidth = ( $('#redTrack').width() );
-//   console.log ("Red track width is: " + redTrackWidth);
-//
-//   //To determine red position's value
-//   var redPlayerMargin = ( parseInt( $('.redPlayer').css('left') ) );
-//   console.log ("Red is currently at: " + redPlayerMargin);
-//
-//
-//   $('.redPlayer').animate ( { 'left' : '+=75'}, 'linear');
-//
-//   if (redPlayerMargin >= redTrackWidth - 400) {
-//     alert('You win, big red pixel!');
-//     $('.pop-up').show(250);
-//   }
+// if (bluePlayerMargin >= blueTrackWidth - 400) {
+//   alert ('You win, big blue pixel!');
+//   $('.pop-up').show(250);
+// }
+
 // }
