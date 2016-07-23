637718f1b0c74c501941855257cd551212047339
hectorcoronado <coronado.hector@gmail.com>
2016-07-22 18:48:24 -0700

    reading pixel position accurately

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 
-console.log ("OK, Javascript is functioning.");
+console.log ("app.js loaded");
 
 
 $(document).on('ready', function() {
 
+
   $(window).keyup (function () {
     if (event.which === 65) {
-      console.log ("Ok, we're listening for A");
-
-      //To determine how far red needs to go
-      var redTrackWidth = ( $('#redTrack').width() );
-      console.log ("Red track width is: " + redTrackWidth);
-
-      //To determine red position's value
-      var redPlayerMargin = ( parseInt( $('.redPlayer').css('left') ) );
-      console.log ("Red is currently at: " + redPlayerMargin);
-
-
-      $('.redPlayer').animate ( { 'left' : '+=75'}, 'linear');
-
-      if (redPlayerMargin >= redTrackWidth - 400) {
-        alert('You win, big red pixel!');
-        $('.pop-up').show(250);
-      }
+      var redPlayerPosition = parseInt( $('.redPlayer').css('left') );
+      console.log ('Red player position is: ' + redPlayerPosition);
+      $('.redPlayer').animate ( { 'left' : '+=20'}, 50);
     }
   });
 
@@ -37,7 +24,7 @@ $(document).on('ready', function() {
 
       //To determine red position's value
       var bluePlayerMargin = ( parseInt( $('.bluePlayer').css('left') ) );
-      console.log ("Red is currently at: " + bluePlayerMargin);
+      console.log ("Blue is currently at: " + bluePlayerMargin);
 
       //Possible solution would be to set winning condition to
       //'position', not to ''
@@ -53,3 +40,22 @@ $(document).on('ready', function() {
   });
 
 });
+
+// extracted logic from redPlayer:
+
+//   //To determine how far red needs to go
+//   var redTrackWidth = ( $('#redTrack').width() );
+//   console.log ("Red track width is: " + redTrackWidth);
+//
+//   //To determine red position's value
+//   var redPlayerMargin = ( parseInt( $('.redPlayer').css('left') ) );
+//   console.log ("Red is currently at: " + redPlayerMargin);
+//
+//
+//   $('.redPlayer').animate ( { 'left' : '+=75'}, 'linear');
+//
+//   if (redPlayerMargin >= redTrackWidth - 400) {
+//     alert('You win, big red pixel!');
+//     $('.pop-up').show(250);
+//   }
+// }
