ad80ae6fdb640097296d3b55b32e623632c5ae8b
hectorcoronado <coronado.hector@gmail.com>
2016-07-24 16:08:21 -0700

    finish line functional

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-
 console.log ("app.js loaded");
 
-
 $(document).on('ready', function() {
+
+var $redPlayer = $('.redPlayer'),
+    $bluePlayer = $('.bluePlayer'),
+    $popUp = $('.pop-up');
 
 
   $(window).keyup (function () {
     if (event.which === 65) {
-      var redPlayerPosition = parseInt( $('.redPlayer').css('left') );
-      console.log ('Red player position is: ' + redPlayerPosition);
-      $('.redPlayer').animate ( { 'left' : '+=20'}, 50);
+      var redPlayerPosition = parseInt( $redPlayer.css('left') );
+      if (redPlayerPosition < 960) {
+        $redPlayer.animate ( { 'left' : '+=20'}, 50);
+      } else {
+        $popUp.show();
+      }
     }
   });
 
 
   $(window).keyup(function () {
     if (event.which === 76) {
-      var bluePlayerPosition = parseInt( $('.bluePlayer').css('left') );
-      console.log ('Blue player position is: ' + bluePlayerPosition);
-      $('.bluePlayer').animate ( { 'left' : '+=20'}, 50);
+      var bluePlayerPosition = parseInt( $bluePlayer.css('left') );
+      if (bluePlayerPosition < 960) {
+        $bluePlayer.animate ( { 'left' : '+=20'}, 50);
+      } else {
+        $popUp.show();
+      }
     }
   });
 
 });
-
-
-// if (bluePlayerMargin >= blueTrackWidth - 400) {
-//   alert ('You win, big blue pixel!');
-//   $('.pop-up').show(250);
-// }
-
-// }
