750bcf7d5fb772c4a73778b41b1598212e032345
hectorcoronado <coronado.hector@gmail.com>
2016-07-24 18:19:21 -0700

    randomized letter to press for red

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-console.log ("app.js loaded");
+console.log("app.js loaded");
 
 $(document).on('ready', function() {
 
-var $redPlayer = $('.redPlayer'),
+  var $redPlayer = $('.redPlayer'),
     $bluePlayer = $('.bluePlayer'),
-    $popUp = $('.pop-up');
+    $popUp = $('.pop-up'),
+    keycode,
+    letter;
 
+  function redRandomLetter() {
+    var num = Math.floor(Math.random() * 4) + 1;
+    if (num === 1) {
+      letter = 'A';
+      keycode = 65;
+    } else if (num === 2) {
+      letter = 'S';
+      keycode = 83;
+    } else if (num === 3) {
+      letter = 'D';
+      keycode = 68;
+    } else if (num === 4) {
+      letter = 'F';
+      keycode = 70;
+    }
+    $redPlayer.append('<p>' + letter + '<p>');
+  }
 
-  $(window).keyup (function () {
-    if (event.which === 65) {
-      var redPlayerPosition = parseInt( $redPlayer.css('left') );
+  redRandomLetter();
+
+  $(window).keyup(function() {
+    if (event.which === keycode) {
+      var redPlayerPosition = parseInt($redPlayer.css('left'));
       if (redPlayerPosition < 960) {
-        $redPlayer.animate ( { 'left' : '+=20'}, 50);
+        $redPlayer.animate({
+          'left': '+=20'
+        }, 50);
+        $redPlayer.empty();
+        redRandomLetter();
       } else {
         $popUp.show();
       }
@@ -19,15 +44,42 @@ var $redPlayer = $('.redPlayer'),
   });
 
 
-  $(window).keyup(function () {
+  $(window).keyup(function() {
     if (event.which === 76) {
-      var bluePlayerPosition = parseInt( $bluePlayer.css('left') );
+      var bluePlayerPosition = parseInt($bluePlayer.css('left'));
       if (bluePlayerPosition < 960) {
-        $bluePlayer.animate ( { 'left' : '+=20'}, 50);
+        $bluePlayer.animate({
+          'left': '+=20'
+        }, 50);
       } else {
         $popUp.show();
       }
     }
   });
+
+
+
+
+  function blueRandomLetter() {
+    var letter;
+    var keycode;
+    if (randomNumber() === 1) {
+      letter = 'J';
+      keycode = 74;
+    } else if (randomNumber() === 2) {
+      letter = 'K';
+      keycode = 75;
+    } else if (randomNumber() === 3) {
+      letter = 'L';
+      keycode = 76;
+    } else if (randomNumber() === 4) {
+      letter = ';';
+      keycode = 186;
+    }
+    return {
+      letter: letter,
+      keycode: keycode
+    };
+  }
 
 });
