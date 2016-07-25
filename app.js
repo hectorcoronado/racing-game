b6426d2785cb04ab970fff3b2bb59e38ab7cbdd3
hectorcoronado <coronado.hector@gmail.com>
2016-07-24 18:28:39 -0700

    random letter to press for both players

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
 console.log("app.js loaded");
 
+
 $(document).on('ready', function() {
+
 
   var $redPlayer = $('.redPlayer'),
     $bluePlayer = $('.bluePlayer'),
     $popUp = $('.pop-up'),
-    keycode,
-    letter;
+    redKeycode,
+    blueKeycode,
+    redLetter,
+    blueLetter;
+
 
   function redRandomLetter() {
     var num = Math.floor(Math.random() * 4) + 1;
     if (num === 1) {
-      letter = 'A';
-      keycode = 65;
+      redLetter = 'A';
+      redKeycode = 65;
     } else if (num === 2) {
-      letter = 'S';
-      keycode = 83;
+      redLetter = 'S';
+      redKeycode = 83;
     } else if (num === 3) {
-      letter = 'D';
-      keycode = 68;
+      redLetter = 'D';
+      redKeycode = 68;
     } else if (num === 4) {
-      letter = 'F';
-      keycode = 70;
+      redLetter = 'F';
+      redKeycode = 70;
     }
-    $redPlayer.append('<p>' + letter + '<p>');
+    $redPlayer.append('<p>' + redLetter + '</p>');
   }
 
+  function blueRandomLetter() {
+    var num = Math.floor(Math.random() * 4) + 1;
+    if (num === 1) {
+      blueLetter = 'J';
+      blueKeycode = 74;
+    } else if (num === 2) {
+      blueLetter = 'K';
+      blueKeycode = 75;
+    } else if (num === 3) {
+      blueLetter = 'L';
+      blueKeycode = 76;
+    } else if (num === 4) {
+      blueLetter = ';';
+      blueKeycode = 186;
+    }
+    $bluePlayer.append('<p>' + blueLetter + '</p>');
+  }
+
+
   redRandomLetter();
+  blueRandomLetter();
 
   $(window).keyup(function() {
-    if (event.which === keycode) {
+    if (event.which === redKeycode) {
       var redPlayerPosition = parseInt($redPlayer.css('left'));
       if (redPlayerPosition < 960) {
         $redPlayer.animate({
@@ -43,43 +68,19 @@ $(document).on('ready', function() {
     }
   });
 
-
   $(window).keyup(function() {
-    if (event.which === 76) {
+    if (event.which === blueKeycode) {
       var bluePlayerPosition = parseInt($bluePlayer.css('left'));
       if (bluePlayerPosition < 960) {
         $bluePlayer.animate({
           'left': '+=20'
         }, 50);
+        $bluePlayer.empty();
+        blueRandomLetter();
       } else {
         $popUp.show();
       }
     }
   });
-
-
-
-
-  function blueRandomLetter() {
-    var letter;
-    var keycode;
-    if (randomNumber() === 1) {
-      letter = 'J';
-      keycode = 74;
-    } else if (randomNumber() === 2) {
-      letter = 'K';
-      keycode = 75;
-    } else if (randomNumber() === 3) {
-      letter = 'L';
-      keycode = 76;
-    } else if (randomNumber() === 4) {
-      letter = ';';
-      keycode = 186;
-    }
-    return {
-      letter: letter,
-      keycode: keycode
-    };
-  }
 
 });
